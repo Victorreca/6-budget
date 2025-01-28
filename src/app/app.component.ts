6b0048f952e0455d059f12e80d7e399be67ee57e
Victor Redondo <vrviktor@gmail.com>
2025-01-28 13:04:21 +0100

    Create Home component and ove budget-list to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BudgetsListComponent } from './budgets-list/budgets-list.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [BudgetsListComponent],
+  imports: [BudgetsListComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { BudgetService } from '../services/budget.service';
+import { BudgetOption } from '../interfaces/budget-option';
+import { PanelComponent } from '../panel/panel.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [ReactiveFormsModule, PanelComponent, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  totalPriceBudget = 0;
+
+  formBudget!: FormGroup;
+  programmingOptions: BudgetOption[] = [];
+
+  private numberPages = 1;
+  private numberLanguages = 1;
+
+  constructor(private fb: FormBuilder, private budgetService: BudgetService) {
+    this.initializeForm();
+  }
+
+  ngOnInit(): void {
+    this.programmingOptions = this.budgetService.getProgrammingOptions();
+  }
+
+  private initializeForm(): void {
+    this.formBudget = this.fb.group({
+      seo: new FormControl(false),
+      ads: new FormControl(false),
+      web: new FormControl(false),
+    });
+  }
+
+  calculateTotal() {
+    this.totalPriceBudget = this.budgetService.calculateTotalPrice(
+      this.formBudget
+    );
+  }
+  panelUpdate(event: { pages: number; languages: number }) {
+    this.numberPages = event.pages;
+    this.numberLanguages = event.languages;
+
+    this.budgetService.updatePages(this.numberPages);
+    this.budgetService.updateLanguages(this.numberLanguages);
+    this.calculateTotal();
+  }
+}
