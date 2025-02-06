4faf20bb24d52c0d38acc7ab01f0af1f3bced563
Victor Redondo <vrviktor@gmail.com>
2025-02-06 16:12:52 +0100

    Add routes query params

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,7 @@ import { PanelComponent } from '../panel/panel.component';
 import { CommonModule } from '@angular/common';
 import { CurrentBudget } from '../interfaces/current-budget';
 import { BudgetsListComponent } from '../budgets-list/budgets-list.component';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -33,11 +34,22 @@ export class HomeComponent implements OnInit {
   private numberPages = 1;
   private numberLanguages = 1;
 
-  constructor(private fb: FormBuilder, private budgetService: BudgetService) {}
+  constructor(
+    private fb: FormBuilder,
+    private budgetService: BudgetService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.programmingOptions = this.budgetService.getProgrammingOptions();
     this.initializeForm();
+
+    this.route.queryParams.subscribe((params) =>
+      this.restoreBudgetFromURL(params)
+    );
+
+    this.formBudget.valueChanges.subscribe(() => this.updateURL());
   }
 
   private initializeForm(): void {
@@ -45,6 +57,8 @@ export class HomeComponent implements OnInit {
       seo: false,
       ads: false,
       web: false,
+      pages: [this.numberPages],
+      languages: [this.numberLanguages],
       name: new FormControl('', [Validators.required]),
       phone: new FormControl('', [
         Validators.required,
@@ -54,7 +68,49 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  calculateTotal() {
+  private updateURL() {
+    const isWebSelected = this.formBudget.get('web')?.value;
+
+    const queryParams: Params = {
+      WebPage: isWebSelected || null,
+      CampaignSeo: this.formBudget.get('seo')?.value || null,
+      Ads: this.formBudget.get('ads')?.value || null,
+      pages: isWebSelected ? this.formBudget.get('pages')?.value : null,
+      lang: isWebSelected ? this.formBudget.get('languages')?.value : null,
+    };
+
+    this.router.navigate([], { queryParams, queryParamsHandling: 'merge' });
+  }
+
+  private restoreBudgetFromURL(params: Params) {
+    const isWebSelected = params['WebPage'] === 'true';
+
+    this.numberPages = isWebSelected
+      ? params['pages']
+        ? +params['pages']
+        : 1
+      : 1;
+    this.numberLanguages = isWebSelected
+      ? params['lang']
+        ? +params['lang']
+        : 1
+      : 1;
+
+    this.formBudget.patchValue({
+      web: isWebSelected,
+      seo: params['CampaignSeo'] === 'true',
+      ads: params['Ads'] === 'true',
+      pages: this.numberPages,
+      languages: this.numberLanguages,
+    });
+
+    this.budgetService.updatePages(this.numberPages);
+    this.budgetService.updateLanguages(this.numberLanguages);
+
+    this.calculateTotal();
+  }
+
+  private calculateTotal() {
     this.totalPriceBudget = this.budgetService.calculateTotalPrice(
       this.formBudget
     );
@@ -62,6 +118,13 @@ export class HomeComponent implements OnInit {
   panelUpdate(event: { pages: number; languages: number }) {
     this.numberPages = event.pages;
     this.numberLanguages = event.languages;
+
+    this.formBudget.patchValue({
+      pages: this.numberPages,
+      languages: this.numberLanguages,
+    });
+
+    this.updateURL();
 
     this.budgetService.updatePages(this.numberPages);
     this.budgetService.updateLanguages(this.numberLanguages);
